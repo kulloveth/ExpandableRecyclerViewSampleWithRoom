02f2ce9529d3d775d3c8219d812abc8e40e5c4d5
Kulloveth <kulloveth@gmail.com>
2020-04-27 03:46:39 +0200

    arrow indicator added

--- app/src/main/java/com/developer/kulloveth/expandablelistsamplewithroom/data/MainViewHoder.kt
+++ app/src/main/java/com/developer/kulloveth/expandablelistsamplewithroom/data/MainViewHoder.kt
@@ -1,6 +1,9 @@
 package com.developer.kulloveth.expandablelistsamplewithroom.data
 
 import android.view.View
+import android.view.animation.Animation.RELATIVE_TO_SELF
+import android.view.animation.RotateAnimation
+import android.widget.ImageView
 import android.widget.TextView
 import com.developer.kulloveth.expandablelistsamplewithroom.R
 import com.developer.kulloveth.expandablelistsamplewithroom.data.model.Continents
@@ -19,8 +22,46 @@ class CountryViewHolder(itemView: View) : ChildViewHolder(itemView) {
 
 class ContinentViewHolder(itemView: View) : GroupViewHolder(itemView) {
     val continentName = itemView.findViewById<TextView>(R.id.continent)
+    val arrow = itemView.findViewById<ImageView>(R.id.arrow)
 
     fun bind(continent: Continents) {
         continentName.text = continent.continentName
     }
+
+    override fun expand() {
+        animateExpand()
+    }
+
+    override fun collapse() {
+        animateCollapse()
+    }
+
+    private fun animateExpand() {
+        val rotate = RotateAnimation(
+            360f,
+            180f,
+            RELATIVE_TO_SELF,
+            0.5f,
+            RELATIVE_TO_SELF,
+            0.5f
+        )
+        rotate.duration = 300
+        rotate.fillAfter = true
+        arrow.animation = rotate
+    }
+
+    private fun animateCollapse() {
+        val rotate = RotateAnimation(
+            180f,
+            360f,
+            RELATIVE_TO_SELF,
+            0.5f,
+            RELATIVE_TO_SELF,
+            0.5f
+        )
+        rotate.duration = 300
+        rotate.fillAfter = true
+        arrow.animation = rotate
+    }
+
 }
